b093c17a0e14031ebd38f397764031eb5591c5a0
neymeka <megustafather@gmail.com>
2018-03-01 00:31:58 +0300

    feacher/master: Zeros

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function getZerosCount(number) {
-  // your implementation
+  var counter = 0;
+  while (number) {
+  number = (number/5) | 0;
+  counter += number;
+  }
+return counter;
 }
